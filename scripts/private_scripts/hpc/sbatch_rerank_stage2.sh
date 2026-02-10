f36362c7252a2ece0bdd3d70fbddc948ce7037c2
Yun Wang <freedom_be@live.cn>
2026-02-10 23:18:56 +0100

    scripts/private_scripts/hpc/sbatch_rerank_stage2.sh

--- scripts/private_scripts/hpc/sbatch_rerank_stage2.sh
+++ scripts/private_scripts/hpc/sbatch_rerank_stage2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J stage2_rerank
-#SBATCH -p frida
-#SBATCH --time=24:00:00
+#SBATCH -p dev
+#SBATCH --time=12:00:00
 #SBATCH --gres=gpu:A100_80GB:4
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=312G
