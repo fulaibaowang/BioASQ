25e6c09d30bb41159432ec6912b4566aa871cd36
Yun Wang <freedom_be@live.cn>
2026-02-18 17:59:56 +0100

    sbatch script

--- scripts/private_scripts/hpc/sbatch_pipeline_3pct.sh
+++ scripts/private_scripts/hpc/sbatch_pipeline_3pct.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J pipeline_3pct
-#SBATCH -p frida
-#SBATCH --time=24:00:00
+#SBATCH -p dev
+#SBATCH --time=12:00:00
 #SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=128G
