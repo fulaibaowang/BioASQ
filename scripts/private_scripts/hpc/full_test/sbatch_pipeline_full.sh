3ea9d83c042627b5afd5cc1808d2dc254a3f60f5
Yun Wang <freedom_be@live.cn>
2026-03-03 16:21:37 +0100

    full test

--- scripts/private_scripts/hpc/full_test/sbatch_pipeline_full.sh
+++ scripts/private_scripts/hpc/full_test/sbatch_pipeline_full.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -J pipeline_full_pubmed_sharded
-#SBATCH -p dev
+#SBATCH -p frida
 #SBATCH --time=24:00:00
-#SBATCH --gres=gpu:A100:1
+#SBATCH --gres=gpu:A100_80GB:1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=256G
 #SBATCH -o logs/%x_%j.out
