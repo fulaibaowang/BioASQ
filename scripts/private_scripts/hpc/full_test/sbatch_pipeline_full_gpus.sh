cf4e82e1c073d2fcff2215bb84f86308b600ed01
Yun Wang <freedom_be@live.cn>
2026-03-05 21:40:59 +0100

    baseline run

--- scripts/private_scripts/hpc/full_test/sbatch_pipeline_full_gpus.sh
+++ scripts/private_scripts/hpc/full_test/sbatch_pipeline_full_gpus.sh
@@ -2,12 +2,11 @@
 #SBATCH -J pipeline_full_pubmed_sharded
 #SBATCH -p dev
 #SBATCH --time=12:00:00
+#SBATCH --gres=gpu:L4:3
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=56G
+#SBATCH --mem=256G
 #SBATCH -o logs/%x_%j.out
 #SBATCH -e logs/%x_%j.err
-
-##SBATCH --gres=gpu:L4:3
 
 set -euo pipefail
 
