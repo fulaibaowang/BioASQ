8d829c896f632fee62cf23bb8784d9f3a2d4ba50
Yun Wang <freedom_be@live.cn>
2026-03-05 21:13:31 +0100

    fisnish baseline run

--- scripts/private_scripts/local/batch_generation_after_tuning.sh
+++ scripts/private_scripts/local/batch_generation_after_tuning.sh
@@ -1,24 +1,24 @@
 python scripts/public/shared_scripts/generation/generate_answers.py \
   --input-path output/workflow_local_10pct_hpc_bge/generation/13B1_golden_answers.json \
-  --output-dir output/workflow_local_10pct_hpc_bge/generation_after_tuning/ \
+  --output-dir output/workflow_local_10pct_hpc_bge/generation_before_tuning/ \
   --sleep 0.5 
 
 python scripts/public/shared_scripts/generation/generate_answers.py \
   --input-path output/workflow_local_10pct_hpc_bge/generation/13B2_golden_answers.json \
-  --output-dir output/workflow_local_10pct_hpc_bge/generation_after_tuning/ \
+  --output-dir output/workflow_local_10pct_hpc_bge/generation_before_tuning/ \
   --sleep 0.5 
 
 python scripts/public/shared_scripts/generation/generate_answers.py \
   --input-path output/workflow_local_10pct_hpc_bge/generation/13B3_golden_answers.json \
-  --output-dir output/workflow_local_10pct_hpc_bge/generation_after_tuning/ \
+  --output-dir output/workflow_local_10pct_hpc_bge/generation_before_tuning/ \
   --sleep 0.5 
 
 python scripts/public/shared_scripts/generation/generate_answers.py \
   --input-path output/workflow_local_10pct_hpc_bge/generation/13B4_golden_answers.json \
-  --output-dir output/workflow_local_10pct_hpc_bge/generation_after_tuning/ \
+  --output-dir output/workflow_local_10pct_hpc_bge/generation_before_tuning/ \
   --sleep 0.5 
 
 python scripts/public/shared_scripts/generation/generate_answers.py \
   --input-path output/workflow_local_10pct_hpc_bge/generation/training14b_10pct_sample_answers.json \
-  --output-dir output/workflow_local_10pct_hpc_bge/generation_after_tuning/ \
+  --output-dir output/workflow_local_10pct_hpc_bge/generation_before_tuning/ \
   --sleep 0.5 
