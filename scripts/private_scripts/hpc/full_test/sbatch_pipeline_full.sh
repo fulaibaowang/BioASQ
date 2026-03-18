1ccebebfd35c8df238e50649905fb2573dc1a579
Yun Wang <freedom_be@live.cn>
2026-03-18 14:23:53 +0100

    listwise rrf test

--- scripts/private_scripts/hpc/full_test/sbatch_pipeline_full.sh
+++ scripts/private_scripts/hpc/full_test/sbatch_pipeline_full.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J pipeline_full_pubmed_sharded
-#SBATCH -p amd
-#SBATCH --time=24:00:00
+#SBATCH -p dev,frida
+#SBATCH --time=12:00:00
 #SBATCH --mem=64G
 #SBATCH -o logs/%x_%j.out
 #SBATCH -e logs/%x_%j.err
