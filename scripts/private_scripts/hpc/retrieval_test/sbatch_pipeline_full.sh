b9edc01dd5eec25ca3f9dd0fda50da03f3019d36
Yun Wang <freedom_be@live.cn>
2026-03-30 10:52:34 +0200

    multi quries fix1

--- scripts/private_scripts/hpc/retrieval_test/sbatch_pipeline_full.sh
+++ scripts/private_scripts/hpc/retrieval_test/sbatch_pipeline_full.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J pipeline_full_pubmed_sharded
-#SBATCH -p dev,frida
-#SBATCH --time=12:00:00
+#SBATCH -p frida
+#SBATCH --time=24:00:00
 #SBATCH --mem=256G
 #SBATCH -o logs/%x_%j.out
 #SBATCH -e logs/%x_%j.err
